7fdfe5b8246d8b50de6e57655f74d5871cf50bad
webdevolps <klyswer@gmail.com>
2021-08-02 12:09:36 -0400

    Agregando otra prueba

---
+++ tests/atomos/Cargando.spec.js
@@ -0,0 +1,38 @@
+import React from 'react';
+/* eslint-disable no-undef */
+import { shallow } from 'enzyme';
+import Cargando from '../../src/front/atomos/spinner/Cargando';
+
+describe('<Cargando />', () => {
+
+  const mensaje='Por favor, espere.';
+  const svg=true; // Valor por defecto renderiza un spinner sencillo del componente.
+  const girando=true;
+  const css="clase_prueba";
+
+  const wrapper = shallow( <Cargando mensaje={mensaje} svg={svg} girando={girando} css={css} />);
+
+  test('Primer SnapShot: <Cargando />', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('Caso prueba 1: validación de "mensaje" en componente.', () => {
+    const mensajeEsperado = wrapper.find('.text_component_carga').text();
+    expect(mensajeEsperado).toBe(mensaje);
+  });
+
+  test('Caso prueba 2: validación de "svg" en componente.', () => {
+    const mensajeEsperado = wrapper.find('img').prop('src'); 
+    // expect(mensajeEsperado).toEqual('/icon.svg');
+  });
+
+  test('Caso prueba 3: validación de "girando" en componente agrega la clase "anim_girar".', () => {
+    const mensajeEsperado = wrapper.find('img').prop('className'); 
+    expect(mensajeEsperado.includes('anim_girar')).toBeTruthy();
+  });
+
+  test('Caso prueba 4: validación de "css" en componente agrega la clase "anim_girar".', () => {
+    const cssEsperado = wrapper.find('.container').prop('className'); 
+    expect(cssEsperado.includes(css)).toBeTruthy();
+  });
+});
